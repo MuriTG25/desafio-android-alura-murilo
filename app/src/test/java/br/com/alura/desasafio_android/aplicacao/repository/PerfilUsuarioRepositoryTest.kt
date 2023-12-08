0eafe5a352db401fca782552ae31935967b374d9
Murilo Bioni Caruso <murilob.programador@gmail.com>
2023-12-08 11:07:53 -0300

    Melhorei o teste de integração depois de ler o manual do Mockk

--- app/src/test/java/br/com/alura/desasafio_android/aplicacao/repository/PerfilUsuarioRepositoryTest.kt
+++ app/src/test/java/br/com/alura/desasafio_android/aplicacao/repository/PerfilUsuarioRepositoryTest.kt
@@ -2,37 +2,36 @@ package br.com.alura.desasafio_android.aplicacao.repository
 
 import br.com.alura.desasafio_android.aplicacao.modelo.mapper.UsuarioMapper
 import br.com.alura.desasafio_android.infraestrutura.webservice.PerfilUsuarioService
-import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.SpyK
+import io.mockk.junit4.MockKRule
 import io.mockk.mockk
 import io.mockk.mockkClass
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.runBlocking
-import org.junit.Before
+import org.amshove.kluent.shouldBeEqualTo
+import org.junit.Rule
 import org.junit.Test
 
 class PerfilUsuarioRepositoryTest {
-
     @MockK
-    var service = mockk<PerfilUsuarioService>()
-    val repositorio by lazy {
-        PerfilUsuarioRepository(service)
-    }
-    @Before
-    fun setUp(){
-        MockKAnnotations.init(this)
-    }
+    private lateinit var service: PerfilUsuarioService
+    @InjectMockKs
+    lateinit var repositorio: PerfilUsuarioRepository
+
+    @get:Rule
+    val mockkRule = MockKRule(this)
     @Test
     fun `Deve chamar pegaUsuario do service, Quando chamar pegaUsuario do repositorio`(): Unit =
         runBlocking {
             coEvery {
                 service.pegaUsuario("")
             } returns mockkClass(UsuarioMapper::class)
-            repositorio.pegaUsuario("").first()
+            repositorio.pegaUsuario("").firstOrNull()
             coVerify {
                 service.pegaUsuario("")
             }
@@ -48,5 +47,4 @@ class PerfilUsuarioRepositoryTest {
             service.pegaListaRepositorio("")
         }
     }
-
 }
